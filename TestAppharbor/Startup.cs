468808a9221fe4cd6e76272fbce207896f85bea1
Baptista <Bruno_baptista86@hotmail.com>
2014-09-30 23:49:48 +0100

    *

--- TestAppharbor/Startup.cs
+++ TestAppharbor/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TestAppharbor.Models;
 
 [assembly: OwinStartupAttribute(typeof(TestAppharbor.Startup))]
 namespace TestAppharbor
@@ -9,6 +13,32 @@ namespace TestAppharbor
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateAdmin();
+        }
+        public void CreateAdmin()
+        {
+            var rm = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            try
+            {
+                if (!rm.RoleExists("admin"))
+                {
+                    rm.Create(new IdentityRole("admin"));
+                    var um = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                    var user = new ApplicationUser() { UserName = "adminn" };
+                    um.Create(user, "asdfgh");
+                    UserLoginInfo info = new UserLoginInfo("Google",
+                            "https://www.google.com/accounts/o8/id?id=AItOawka6ZSrKNn7UY3ZUcjFRZMSLhMqQNKArWQ");
+                    um.AddToRole(user.Id, "admin");
+                    um.AddLogin(user.Id, info);
+
+                }
+            }
+            catch (TimeoutException)
+            {
+                CreateAdmin();
+
+            }
+
         }
     }
 }
